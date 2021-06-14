5e3f6dc8fbd15a6af61fadc9d6308b333b1281fd
Alan Balcom <alanbalcom@Alans-MacBook-Pro.local>
2021-06-13 21:33:46 -0400

    trying to get more login and comment logic working

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -6,7 +6,7 @@ async function loginFormHandler(event) {
   
     if (email && password) {
       const response = await fetch('/api/users/login', {
-        method: 'post',
+        method: 'POST',
         body: JSON.stringify({
           email,
           password
@@ -18,6 +18,7 @@ async function loginFormHandler(event) {
         document.location.replace('/dashboard');
       } else {
         alert(response.statusText);
+        console.log('ERROR!!!!!')
       }
     }
   }
